a27efa81748302d59c90c2897c0d37623c6a57ef
dipGhost <deepghosh@pandatoto.in>
2024-05-08 01:08:08 +0530

    Fix | change schema and seed database

--- prisma/db/seed-movies.ts
+++ prisma/db/seed-movies.ts
@@ -1,53 +1,100 @@
 const bcrypt = require('bcrypt');
-const { PrismaClient, Genre } = require('@prisma/client');
+const { PrismaClient, Genre, ContentType } = require('@prisma/client');
 
 async function seedMoviews() {
   const prisma = new PrismaClient();
 
   try {
-    const movies = [];
-    for (let i = 1; i <= 15; i++) {
-      movies.push({
-        title: `Movie ${i}`,
-        description: `Description of Movie ${i}`,
-        genres: { set: [Genre.Action, Genre.SciFi] },
-        releaseDate: new Date(`2023-01-${i < 10 ? '0' + i : i}`),
-        director: `Director ${i}`,
-        actors: { set: [`Actor ${i}`, `Actor ${i + 1}`] }
-      });
-    }
+    const content = [];
 
-    const tvShows = [];
-    for (let i = 1; i <= 15; i++) {
-      tvShows.push({
-        title: `TV Show ${i}`,
-        description: `Description of TV Show ${i}`,
-        genres: { set: [Genre.Comedy, Genre.Drama] },
-      });
-    }
 
-    // const episodes = [];
-    // for (let i = 1; i <= 30; i++) {
-    //   episodes.push({
-    //     episodeNumber: i,
-    //     seasonNumber: Math.ceil(i / 10),
-    //     releaseDate: new Date(`2022-05-${i < 10 ? '0' + i : i}`),
-    //     director: `Director ${i}`,
-    //     actors: { set: [`Actor ${i}`, `Actor ${i + 1}`] }
+    // const tvShows = [];
+    // for (let i = 1; i <= 500; i++) {
+    //   tvShows.push({
+    //     title: `TV Show ${i}`,
+    //     description: `Description of TV Show ${i}`,
+    //     genres: { set: [Genre.Comedy, Genre.Drama] },
+    //     episodes: {
+    //         createMany: {
+    //             data: Array.from({ length: 20 }, (_, i) => ({
+    //               episodeNumber: i + 1,
+    //               seasonNumber: Math.ceil((i + 1) / 10),
+    //               releaseDate: new Date(`2022-05-${i + 1 < 10 ? '0' + (i + 1) : (i + 1)}`),
+    //               director: `Director ${i + 1}`,
+    //               actors: { set: [`Actor ${i + 1}`, `Actor ${i + 2}`] },
+    //             })),
+    //         }
+    //     }
     //   });
     // }
-
-    // Initialize Prisma client
     const prisma = new PrismaClient();
 
-    // Seed movies
-    // for (const movieData of movies) {
-    //   await prisma.movie.create({ data: movieData });
+    // Seed TV shows
+    // for (const tvShowData of tvShows) {
+    //   await prisma.tVShow.create({ data: tvShowData });
     // }
 
-    // Seed TV shows
-    for (const tvShowData of tvShows) {
-      await prisma.tVShow.create({ data: tvShowData });
+    // content.push({
+    //   title: `Movie ${i}`,
+    //   description: `Description of Movie ${i}`,
+    //   genres: { set: [Genre.Action, Genre.SciFi] },
+    //   releaseDate: new Date(`2023-01-${i < 10 ? '0' + i : i}`),
+    //   director: `Director ${i}`,
+    //   actors: { set: [`Actor ${i}`, `Actor ${i + 1}`] }
+    // });
+
+
+      // const actors = await prisma.director.createMany({
+      //   data: Array.from({ length: 20 }, (_, i) => ({
+      //     name: `director ${i}`
+      //   }))
+      // })
+
+    for (let i = 1; i <= 100; i++) {
+      const content = await prisma.content.create({ data: {
+          title: `Movie ${i}`,
+          description: `Description of Movie ${i}`,
+          genres: { set: [Genre.Action, Genre.SciFi] },
+          releaseDate: new Date(`2023-01-${(Math.floor(Math.random() * (30 - 10 + 1)) + 10)}`),
+          type: ContentType.TVShow,
+          director: {
+            connect: {
+              id: "663a7cbd862fe5fae5222c43"
+            }
+          },
+          episodes: {
+            createMany: {
+              data: Array.from({ length: 15 }, (_, i) => ({
+                episodeNumber: i + 1,
+                seasonNumber: Math.ceil((i + 1) / 10),
+              })),
+          }
+          }
+        }
+      });
+
+      await prisma.ContentActor.create({data: {
+        Content: {
+          connect: {
+            id: content.id
+          }
+        },
+        actor: {
+          connect: {id: "663a7c9b3e835444350a9697"}
+        }
+      }})
+
+      await prisma.ContentActor.create({data: {
+        Content: {
+          connect: {
+            id: content.id
+          }
+        },
+        actor: {
+          connect: {id: "663a7c9b3e835444350a9699"}
+        }
+      }})
+      
     }
 
     console.log('Data seeded successfully!');
